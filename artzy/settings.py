bc45c15a52d828a1df4b3e1d318d769e84b9ad07
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-03-08 12:14:07 +0000

    add home app and templates

--- artzy/settings.py
+++ artzy/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     #'allauth.socialaccount.providers.facebook',
     #'allauth.socialaccount.providers.google',
+    'home',
 
 ]
 
@@ -61,7 +62,10 @@ ROOT_URLCONF = 'artzy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
