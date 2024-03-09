87aac2c94e82292d6457a60bf805f48be24f65e9
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-03-09 12:11:21 +0000

    customize admin for categories and products

--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Slightly modified model by adding description
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
