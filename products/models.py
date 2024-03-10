d1843ad5b7c53dbc08ee6d13d840bc0b9f7557ae
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-03-10 13:52:28 +0000

    Add artist to product model

--- products/models.py
+++ products/models.py
@@ -17,11 +17,13 @@ class Category(models.Model):
         return self.friendly_name
 
 
+# Minor adjusment of model by adding artist
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    artist = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
