cf82c93c237f6538bf93a7299d0eae93a48801e7
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-03-12 13:11:08 +0000

    add notification css, shopping cart preview and additional messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse,  get_object_or_404
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -10,6 +13,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     print_type = None
@@ -21,15 +25,20 @@ def add_to_cart(request, item_id):
         if item_id in list(cart.keys()):
             if print_type in cart[item_id]['items_by_print'].keys():
                 cart[item_id]['items_by_print'][print_type] += quantity
+                messages.success(request, f'Updated print {print_type} {product.name} quantity to {cart[item_id]["items_by_print"][print_type]}')
             else:
                 cart[item_id]['items_by_print'][print_type] = quantity
+                messages.success(request, f'Added print {print_type} {product.name} to your cart')
         else:
             cart[item_id] = {'items_by_print': {print_type: quantity}}
+            messages.success(request, f'Added print {print_type} {product.name} to your cart')
     else:
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
+            messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
         else:
             cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -38,6 +47,7 @@ def add_to_cart(request, item_id):
 def adjust_cart(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     print_type = None
     if 'print_paper' in request.POST:
@@ -47,6 +57,7 @@ def adjust_cart(request, item_id):
     if print_type:
         if quantity > 0:
             cart[item_id]['items_by_print'][print_type] = quantity
+            messages.success(request, f'Updated print {print_type} {product.name} quantity to {cart[item_id]["items_by_print"][print_type]}')
         else:
             del cart[item_id]['items_by_print'][print_type]
             if not cart[item_id]['items_by_print']:
@@ -54,8 +65,10 @@ def adjust_cart(request, item_id):
     else:
         if quantity > 0:
             cart[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
         else:
             cart.pop(item_id)
+            messages.success(request, f'Removed print {print_type} {product.name} from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -65,6 +78,7 @@ def remove_from_cart(request, item_id):
     """Remove the item from the shopping cart"""
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         print_type = None
         if 'print_paper' in request.POST:
             print_type = request.POST['print_paper']
@@ -74,11 +88,14 @@ def remove_from_cart(request, item_id):
             del cart[item_id]['items_by_print'][print_type]
             if not cart[item_id]['items_by_print']:
                 cart.pop(item_id)
+                messages.success(request, f'Removed print {print_type} {product.name} from your cart')
         else:
             cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
