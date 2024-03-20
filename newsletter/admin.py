48c86edf8763a2fa5fab177ee0bd40a16a93c693
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-03-20 07:58:07 +0000

    add to admin panel for newsletter and profiles app

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Subscriber, Newsletter
 
 # Register your models here.
+
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = (
+        'email', 'name'
+    )
+
+    ordering = ('name',)
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'created_on',
+    )
+
+admin.site.register(Subscriber, SubscriberAdmin)
+admin.site.register(Newsletter, NewsletterAdmin)
